562a99d94f14e3eb09d388b371dea85ac5a2dec1
Y.LEE <y.lee@101.inc>
2023-07-12 11:51:34 +0900

    composeView 가 기존 theme 을 공유받도록 MdcTheme 라이브러리 설치 및 셋팅 수정

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,7 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.content.res.Configuration
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.compose.foundation.layout.Column
@@ -39,6 +40,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.accompanist.themeadapter.material.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -82,7 +84,20 @@ private fun PlantDetailPreview() {
         description = "HTML<br><br>description<br>APPLE",
         growZoneNumber = 7, wateringInterval = 30
     )
-    MaterialTheme{
+    MdcTheme{
+        PlantDetailContent(plant = plant)
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PlantDetailDarkPreview() {
+    val plant = Plant(
+        plantId = "id", name = "Apple",
+        description = "HTML<br><br>description<br>APPLE",
+        growZoneNumber = 7, wateringInterval = 30
+    )
+    MdcTheme{
         PlantDetailContent(plant = plant)
     }
 }
@@ -116,7 +131,7 @@ fun PlantWatering(wateringInterval : Int) {
 @Preview
 @Composable
 private fun PlantWateringPreview() {
-    MaterialTheme{
+    MdcTheme{
         PlantWatering(wateringInterval = 20)
     }
 }
@@ -141,7 +156,7 @@ private fun PlantDescription(description : String) {
 @Preview
 @Composable
 private fun PlantDescriptionPreview() {
-    MaterialTheme {
+    MdcTheme {
         PlantDescription(description = "HTML<br><br>description")
     }
 }
